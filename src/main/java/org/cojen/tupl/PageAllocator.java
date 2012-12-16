6e2affaeeadfbb0fa2e76c7f29b812f44bce4808
broneill <bronee@gmail.com>
2012-12-16 12:07:06 -0800

    Tie page dirtying with checkpointing, to throttle writing threads.

--- src/main/java/org/cojen/tupl/PageAllocator.java
+++ src/main/java/org/cojen/tupl/PageAllocator.java
@@ -16,6 +16,7 @@
 
 package org.cojen.tupl;
 
+import java.io.InterruptedIOException;
 import java.io.IOException;
 
 /**
@@ -25,17 +26,24 @@ import java.io.IOException;
  * @author Brian S O'Neill
  */
 final class PageAllocator {
-    private final PageDb mSource;
+    private final PageDb mPageDb;
+    private final Latch mLatch;
+    private final WaitQueue mWaitQueue;
 
     // Linked list of dirty nodes.
     private Node mFirstDirty;
     private Node mLastDirty;
 
     // Iterator over dirty nodes.
-    private Node mIterateNext;
+    private Node mFlushNext;
+    private int mFlushDirtyState;
+    private int mAltFlushingCount;
+    private IOException mAltFlushFailure;
 
     PageAllocator(PageDb source) {
-        mSource = source;
+        mPageDb = source;
+        mLatch = new Latch();
+        mWaitQueue = new WaitQueue();
     }
 
     /**
@@ -44,83 +52,165 @@ final class PageAllocator {
     long allocPage(Node forNode) throws IOException {
         // When allocations are in order, the list maintains the order.
         dirty(forNode);
-        return mSource.allocPage();
+        return mPageDb.allocPage();
     }
 
     /**
      * Move or add node to the end of the dirty list.
      */
-    synchronized void dirty(Node node) {
-        final Node next = node.mNextDirty;
-        final Node prev = node.mPrevDirty;
-        if (next != null) {
-            if ((next.mPrevDirty = prev) == null) {
-                mFirstDirty = next;
-            } else {
-                prev.mNextDirty = next;
+    void dirty(Node node) {
+        final Latch latch = mLatch;
+        latch.acquireExclusive();
+        try {
+            // If a flush is in progress, throttle any threads which are
+            // dirtying more nodes, by forcing them to do some of the work.
+            int dirtyState = mFlushDirtyState;
+            if (dirtyState != 0) {
+                try {
+                    flushNextDirtyNode(dirtyState, false);
+                } catch (IOException e) {
+                    // Should not be thrown when flusher argument is false.
+                    throw Utils.rethrow(e);
+                }
+                latch.acquireExclusive();
             }
-            node.mNextDirty = null;
-            (node.mPrevDirty = mLastDirty).mNextDirty = node;
-        } else if (prev == null) {
-            Node last = mLastDirty;
-            if (last == node) {
-                return;
+
+            final Node next = node.mNextDirty;
+            final Node prev = node.mPrevDirty;
+            if (next != null) {
+                if ((next.mPrevDirty = prev) == null) {
+                    mFirstDirty = next;
+                } else {
+                    prev.mNextDirty = next;
+                }
+                node.mNextDirty = null;
+                (node.mPrevDirty = mLastDirty).mNextDirty = node;
+            } else if (prev == null) {
+                Node last = mLastDirty;
+                if (last == node) {
+                    return;
+                }
+                if (last == null) {
+                    mFirstDirty = node;
+                } else {
+                    node.mPrevDirty = last;
+                    last.mNextDirty = node;
+                }
             }
-            if (last == null) {
-                mFirstDirty = node;
-            } else {
-                node.mPrevDirty = last;
-                last.mNextDirty = node;
+            mLastDirty = node;
+            // See flushNextDirtyNode for explanation for node latch requirement.
+            if (mFlushNext == node) {
+                mFlushNext = next;
             }
-        }
-        mLastDirty = node;
-        // See removeNextDirtyNode for explanation for node latch requirement.
-        if (mIterateNext == node) {
-            mIterateNext = next;
+        } finally {
+            latch.releaseExclusive();
         }
     }
 
     void recyclePage(long id) throws IOException {
-        mSource.recyclePage(id);
+        mPageDb.recyclePage(id);
     }
 
     /**
-     * Begin iterating over the dirty node list. Only one iteration at a time
-     * is allowed.
+     * Flush all nodes matching the given state. Only one flush at a time is
+     * allowed.
      */
-    synchronized void beginDirtyIteration() {
-        mIterateNext = mFirstDirty;
-    }
+    void flushDirtyNodes(int dirtyState) throws IOException {
+        final Latch latch = mLatch;
+        latch.acquireExclusive();
+        mFlushNext = mFirstDirty;
+        mFlushDirtyState = dirtyState;
+        latch.releaseExclusive();
 
-    /**
-     * Iterate to the next dirty node, and remove it from the dirty
-     * list. Returned Nodes are latched exclusively; caller is responsible for
-     * unlatching.
-     *
-     * @param dirtyState node must match this state
-     */
-    Node removeNextDirtyNode(int dirtyState) {
-        Node node;
-        while (true) {
-            synchronized (this) {
-                node = mIterateNext;
-                if (node == null) {
-                    return null;
+        do {
+            latch.acquireExclusive();
+        } while (flushNextDirtyNode(dirtyState, true));
+
+        latch.acquireExclusive();
+        try {
+            // Wait for concurrent writes to complete.
+            while (mAltFlushingCount != 0) {
+                if (mWaitQueue.await(latch, new WaitQueue.Node(), -1, 0) < 0) {
+                    throw new InterruptedIOException();
                 }
-                mIterateNext = node.mNextDirty;
             }
+            IOException ex = mAltFlushFailure;
+            if (ex != null) {
+                mAltFlushFailure = null;
+                throw ex;
+            }
+        } finally {
+            latch.releaseExclusive();
+        }
+    }
 
-            node.acquireExclusive();
+    /**
+     * Iterate to the next dirty node, remove it from the dirty list, and then
+     * flush it. Caller must have acquired latch, which is released by this
+     * method.
+     *
+     * @param dirtyState iteration and node must match this state
+     * @param flusher pass true if called by main flushing thread; IOException
+     * is not thrown from this method when false
+     * @return false if iteration is complete
+     */
+    private boolean flushNextDirtyNode(int dirtyState, boolean flusher) throws IOException {
+        Node node;
+        final Latch latch;
+        try {
+            node = mFlushNext;
+            if (node == null) {
+                mFlushDirtyState = 0;
+                return false;
+            }
+            if (!flusher) {
+                if (!node.tryAcquireExclusive()) {
+                    // Deadlock avoidance.
+                    return true;
+                }
+                mAltFlushingCount++;
+            }
+            mFlushNext = node.mNextDirty;
+        } finally {
+            (latch = mLatch).releaseExclusive();
+        }
+
+        while (true) {
+            if (flusher) {
+                node.acquireExclusive();
+            }
             if (node.mCachedState == dirtyState) {
                 break;
             }
             node.releaseExclusive();
+
+            latch.acquireExclusive();
+            try {
+                node = mFlushNext;
+                if (node == null) {
+                    mFlushDirtyState = 0;
+                    if (!flusher && --mAltFlushingCount == 0) {
+                        mWaitQueue.signal();
+                    }
+                    return false;
+                }
+                if (!flusher && !node.tryAcquireExclusive()) {
+                    if (--mAltFlushingCount == 0) {
+                        mWaitQueue.signal();
+                    }
+                    return true;
+                }
+                mFlushNext = node.mNextDirty;
+            } finally {
+                latch.releaseExclusive();
+            }
         }
 
         // Remove from list. Because allocPage requires nodes to be latched,
-        // there's no need to update mIterateNext. The removed node will never
-        // be the same as mIterateNext.
-        synchronized (this) {
+        // there's no need to update mFlushNext. The removed node will never be
+        // the same as mFlushNext.
+        latch.acquireExclusive();
+        try {
             Node next = node.mNextDirty;
             Node prev = node.mPrevDirty;
             if (next != null) {
@@ -135,24 +225,67 @@ final class PageAllocator {
             } else if (mFirstDirty == node) {
                 mFirstDirty = next;
             }
+        } finally {
+            latch.releaseExclusive();
         }
 
-        return node;
+        IOException ex = null;
+        try {
+            node.downgrade();
+            try {
+                node.write(mPageDb);
+                // Clean state must be set after write completes. Although latch
+                // has been downgraded to shared, modifying the state is safe
+                // because no other thread could have changed it. This is because
+                // the exclusive latch was acquired first. Releasing the shared
+                // latch performs a volatile write, and so the state change gets
+                // propagated correctly.
+                node.mCachedState = Node.CACHED_CLEAN;
+            } finally {
+                node.releaseShared();
+            }
+        } catch (IOException e) {
+            if (flusher) {
+                throw e;
+            }
+            ex = e;
+        }
+
+        if (!flusher) {
+            latch.acquireExclusive();
+            try {
+                if (ex != null && mAltFlushFailure != null) {
+                    mAltFlushFailure = ex;
+                }
+                if (--mAltFlushingCount == 0) {
+                    mWaitQueue.signal();
+                }
+            } finally {
+                latch.releaseExclusive();
+            }
+        }
+
+        return true;
     }
 
     /**
      * Remove all nodes from dirty list, as part of close sequence.
      */
-    synchronized void clearDirtyNodes() {
-        Node node = mFirstDirty;
-        mIterateNext = null;
-        mFirstDirty = null;
-        mLastDirty = null;
-        while (node != null) {
-            Node next = node.mNextDirty;
-            node.mPrevDirty = null;
-            node.mNextDirty = null;
-            node = next;
+    void clearDirtyNodes() {
+        mLatch.acquireExclusive();
+        try {
+            Node node = mFirstDirty;
+            mFlushNext = null;
+            mFirstDirty = null;
+            mLastDirty = null;
+            while (node != null) {
+                Node next = node.mNextDirty;
+                node.mPrevDirty = null;
+                node.mNextDirty = null;
+                node = next;
+            }
+        } finally {
+            mLatch.releaseExclusive();
         }
     }
 }
