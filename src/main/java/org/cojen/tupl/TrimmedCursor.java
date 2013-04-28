42170b91559c6c21da9a5c05525dac0c56fa9203
broneill <bronee@gmail.com>
2013-04-28 14:10:11 -0700

    Added prefix view support.

---
+++ src/main/java/org/cojen/tupl/TrimmedCursor.java
@@ -0,0 +1,198 @@
+/*
+ *  Copyright 2013 Brian S O'Neill
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.cojen.tupl;
+
+import java.io.IOException;
+
+/**
+ * 
+ *
+ * @author Brian S O'Neill
+ */
+class TrimmedCursor implements Cursor {
+    private final Cursor mSource;
+    private final int mTrim;
+
+    private byte[] mKey;
+
+    TrimmedCursor(Cursor source, int trim) {
+        mSource = source;
+        mTrim = trim;
+    }
+
+    @Override
+    public Transaction link(Transaction txn) {
+        return mSource.link(txn);
+    }
+
+    @Override
+    public byte[] key() {
+        byte[] key = mKey;
+        if (key == null) {
+            byte[] full = mSource.key();
+            if (full != null) {
+                int trim = mTrim;
+                int len = full.length - trim;
+                key = new byte[len];
+                System.arraycopy(full, trim, key, 0, len);
+                mKey = key;
+            }
+        }
+        return key;
+    }
+
+    @Override
+    public byte[] value() {
+        return mSource.value();
+    }
+
+    @Override
+    public boolean autoload(boolean mode) {
+        return mSource.autoload(mode);
+    }
+
+    @Override
+    public int compareKeyTo(byte[] rkey) {
+        byte[] lkey = mKey;
+        return Utils.compareKeys(lkey, 0, lkey.length, rkey, 0, rkey.length);
+    }
+
+    @Override
+    public int compareKeyTo(byte[] rkey, int offset, int length) {
+        byte[] lkey = mKey;
+        return Utils.compareKeys(lkey, 0, lkey.length, rkey, offset, length);
+    }
+
+    @Override
+    public LockResult first() throws IOException {
+        mKey = null;
+        return mSource.first();
+    }
+
+    @Override
+    public LockResult last() throws IOException {
+        mKey = null;
+        return mSource.last();
+    }
+
+    @Override
+    public LockResult skip(long amount) throws IOException {
+        mKey = null;
+        return mSource.skip(amount);
+    }
+
+    @Override
+    public LockResult next() throws IOException {
+        mKey = null;
+        return mSource.next();
+    }
+
+    @Override
+    public LockResult nextLe(byte[] limitKey) throws IOException {
+        mKey = null;
+        return mSource.nextLe(limitKey);
+    }
+
+    @Override
+    public LockResult nextLt(byte[] limitKey) throws IOException {
+        mKey = null;
+        return mSource.nextLt(limitKey);
+    }
+
+    @Override
+    public LockResult previous() throws IOException {
+        mKey = null;
+        return mSource.previous();
+    }
+
+    @Override
+    public LockResult previousGe(byte[] limitKey) throws IOException {
+        mKey = null;
+        return mSource.previousGe(limitKey);
+    }
+
+    @Override
+    public LockResult previousGt(byte[] limitKey) throws IOException {
+        mKey = null;
+        return mSource.previousGt(limitKey);
+    }
+
+    @Override
+    public LockResult find(byte[] key) throws IOException {
+        mKey = null;
+        return mSource.find(key);
+    }
+
+    @Override
+    public LockResult findGe(byte[] key) throws IOException {
+        mKey = null;
+        return mSource.findGe(key);
+    }
+
+    @Override
+    public LockResult findGt(byte[] key) throws IOException {
+        mKey = null;
+        return mSource.findGt(key);
+    }
+
+    @Override
+    public LockResult findLe(byte[] key) throws IOException {
+        mKey = null;
+        return mSource.findLe(key);
+    }
+
+    @Override
+    public LockResult findLt(byte[] key) throws IOException {
+        mKey = null;
+        return mSource.findLt(key);
+    }
+
+    @Override
+    public LockResult findNearby(byte[] key) throws IOException {
+        mKey = null;
+        return mSource.findNearby(key);
+    }
+
+    @Override
+    public LockResult random(byte[] lowKey, byte[] highKey) throws IOException {
+        mKey = null;
+        return mSource.random(lowKey, highKey);
+    }
+
+    @Override
+    public LockResult load() throws IOException {
+        return mSource.load();
+    }
+
+    @Override
+    public void store(byte[] value) throws IOException {
+        mSource.store(value);
+    }
+
+    @Override
+    public Cursor copy() {
+        TrimmedCursor c = new TrimmedCursor(mSource.copy(), mTrim);
+        c.mKey = mKey;
+        return c;
+    }
+
+    @Override
+    public void reset() {
+        mKey = null;
+        mSource.reset();
+    }
+}
