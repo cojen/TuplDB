107fadc2fc28e24887ef6b326df51bf4968abe41
broneill <bronee@gmail.com>
2013-04-28 08:49:22 -0700

    Added support for memory mapped I/O.

---
+++ src/main/java/org/cojen/tupl/io/NioMapping.java
@@ -0,0 +1,95 @@
+/*
+ *  Copyright 2013 Brian S O'Neill
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.cojen.tupl.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import java.lang.reflect.Method;
+
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+
+import java.nio.channels.FileChannel;
+import static java.nio.channels.FileChannel.MapMode.*;
+
+/**
+ * 
+ *
+ * @author Brian S O'Neill
+ */
+class NioMapping extends Mapping {
+    private static volatile boolean cUnmapUnsupported;
+
+    private final RandomAccessFile mRaf;
+    private final FileChannel mChannel;
+    private final MappedByteBuffer mBuffer;
+
+    NioMapping(File file, boolean readOnly, long position, int size) throws IOException {
+        mRaf = new RandomAccessFile(file, readOnly ? "r" : "rw");
+        mChannel = mRaf.getChannel();
+        mBuffer = mChannel.map(readOnly ? READ_ONLY : READ_WRITE, position, size);
+    }
+
+    @Override
+    void read(int start, byte[] b, int off, int len) {
+        ByteBuffer bb = mBuffer.slice();
+        bb.position(start);
+        bb.get(b, off, len);
+    }
+
+    @Override
+    void write(int start, byte[] b, int off, int len) {
+        ByteBuffer bb = mBuffer.slice();
+        bb.position(start);
+        bb.put(b, off, len);
+    }
+
+    @Override
+    void sync(boolean metadata) throws IOException {
+        mBuffer.force();
+        mChannel.force(metadata);
+    }
+
+    @Override
+    public void close() throws IOException {
+        // Workaround to unmap the file.
+        // http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4724038
+        if (!cUnmapUnsupported) {
+            try {
+                Method m = mBuffer.getClass().getMethod("cleaner");
+                if (m != null) {
+                    m.setAccessible(true);
+                    Object cleaner = m.invoke(mBuffer);
+                    if (cleaner != null) {
+                        m = cleaner.getClass().getMethod("clean");
+                        if (m != null) {
+                            m.setAccessible(true);
+                            m.invoke(cleaner);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                // Cannot be unmapped, so leak memory instead.
+                cUnmapUnsupported = true;
+            }
+        }
+
+        mRaf.close();
+    }
+}
