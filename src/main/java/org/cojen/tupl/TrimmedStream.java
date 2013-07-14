1ce056b02841e68d12af98fddacc1ac58bff3f8a
broneill <bronee@gmail.com>
2013-07-14 14:06:46 -0700

    Add trimmed (key) stream.

---
+++ src/main/java/org/cojen/tupl/TrimmedStream.java
@@ -0,0 +1,74 @@
+/*
+ *  Copyright 2013 Brian S O'Neill
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.cojen.tupl;
+
+import java.io.IOException;
+
+/**
+ * 
+ *
+ * @author Brian S O'Neill
+ */
+class TrimmedStream extends Stream {
+    private final TrimmedView mView;
+    private final Stream mSource;
+
+    TrimmedStream(TrimmedView view, Stream source) {
+        mView = view;
+        mSource = source;
+    }
+
+    @Override
+    public LockResult open(Transaction txn, byte[] key) throws IOException {
+        return mSource.open(txn, mView.applyPrefix(key));
+    }
+
+    @Override
+    public long length() throws IOException {
+        return mSource.length();
+    }
+
+    @Override
+    public void setLength(long length) throws IOException {
+        mSource.setLength(length);
+    }
+
+    @Override
+    int doRead(long pos, byte[] buf, int off, int len) throws IOException {
+        return mSource.doRead(pos, buf, off, len);
+    }
+
+    @Override
+    void doWrite(long pos, byte[] buf, int off, int len) throws IOException {
+        mSource.doWrite(pos, buf, off, len);
+    }
+
+    @Override
+    int pageSize() {
+        return mSource.pageSize();
+    }
+
+    @Override
+    void checkOpen() {
+        mSource.checkOpen();
+    }
+
+    @Override
+    void doClose() {
+        mSource.close();
+    }
+}
