cf48afd3c94d2e1b49b2e827d4d25b8171b9d29e
broneill <bronee@gmail.com>
2013-12-21 13:17:12 -0800

    Added compaction abort tests.

--- src/test/java/org/cojen/tupl/CompactTest.java
+++ src/test/java/org/cojen/tupl/CompactTest.java
@@ -186,4 +186,140 @@ public class CompactTest {
             }
         }
     }
+
+    @Test
+    public void manualAbort() throws Exception {
+        mDb = newTempDatabase();
+
+        final Index ix = mDb.openIndex("test");
+        final int seed = 98232;
+        final int count = 100_000;
+
+        Random rnd = new Random(seed);
+        for (int i=0; i<count; i++) {
+            int k = rnd.nextInt();
+            byte[] key = ("key" + k).getBytes();
+            ix.store(Transaction.BOGUS, key, key);
+        }
+
+        rnd = new Random(seed);
+        for (int i=0; i<count; i++) {
+            int k = rnd.nextInt();
+            if (i % 4 != 0) {
+                byte[] key = ("key" + k).getBytes();
+                ix.delete(Transaction.BOGUS, key);
+            }
+        }
+
+        Database.Stats stats1 = mDb.stats();
+
+        CompactionObserver obs = new CompactionObserver() {
+            private int count;
+
+            @Override
+            public boolean indexNodeVisited(long id) {
+                return ++count < 100;
+            }
+        };
+
+        boolean result = mDb.compact(obs, 0.5);
+        assertFalse(result);
+
+        Database.Stats stats2 = mDb.stats();
+        assertEquals(stats1, stats2);
+
+        assertTrue(mDb.verify(null));
+    }
+
+    @Test
+    public void autoAbort() throws Exception {
+        mDb = newTempDatabase();
+
+        final Index ix = mDb.openIndex("test");
+        final int seed = 98232;
+        final int count = 100_000;
+
+        Random rnd = new Random(seed);
+        for (int i=0; i<count; i++) {
+            int k = rnd.nextInt();
+            byte[] key = ("key" + k).getBytes();
+            ix.store(Transaction.BOGUS, key, key);
+        }
+
+        rnd = new Random(seed);
+        for (int i=0; i<count; i++) {
+            int k = rnd.nextInt();
+            if (i % 4 != 0) {
+                byte[] key = ("key" + k).getBytes();
+                ix.delete(Transaction.BOGUS, key);
+            }
+        }
+
+        class Obs extends CompactionObserver {
+            private int count;
+            private boolean resume;
+
+            @Override
+            public synchronized boolean indexNodeVisited(long id) {
+                if (++count > 100) {
+                    try {
+                        while (!resume) {
+                            wait();
+                        }
+                    } catch (InterruptedException e) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            synchronized void resume() {
+                this.resume = true;
+                notify();
+            }
+        };
+
+        final Obs obs = new Obs();
+
+        class Compactor extends Thread {
+            volatile Object result;
+
+            @Override
+            public void run() {
+                try {
+                    result = mDb.compact(obs, 0.5);
+                } catch (Exception e) {
+                    result = e;
+                }
+            }
+        };
+
+        Compactor comp = new Compactor();
+        comp.start();
+
+        // Compaction will abort because of database growth.
+        rnd = new Random(seed);
+        for (int i=0; i<count; i++) {
+            int k = rnd.nextInt();
+            if (i % 4 != 0) {
+                byte[] key = ("key" + k).getBytes();
+                ix.insert(Transaction.BOGUS, key, key);
+            }
+        }
+
+        obs.resume();
+        comp.join();
+
+        assertEquals(Boolean.FALSE, comp.result);
+
+        assertTrue(mDb.verify(null));
+
+        rnd = new Random(seed);
+        for (int i=0; i<count; i++) {
+            int k = rnd.nextInt();
+            byte[] key = ("key" + k).getBytes();
+            byte[] value = ix.load(Transaction.BOGUS, key);
+            fastAssertArrayEquals(key, value);
+        }
+    }
 }
