4cc623937b555b1c2e1fbc79c2329f5b88f01a0e
broneill <bronee@gmail.com>
2014-10-15 09:23:15 -0700

    Fix for deadlock with shared commit lock.

--- src/main/java/org/cojen/tupl/Database.java
+++ src/main/java/org/cojen/tupl/Database.java
@@ -2803,7 +2803,12 @@ public final class Database implements CauseCloseable {
     }
 
     /**
-     * Access the shared commit lock, which prevents commits while held.
+     * Access the shared commit lock, which prevents commits while held. In general, it should
+     * be acquired before any node latches, but postponing acquisition reduces the total time
+     * held. Checkpoints don't have to wait as long for the exclusive commit lock. Because node
+     * latching first isn't the canonical ordering, acquiring the shared commit lock later must
+     * be prepared to abort. Try to acquire first, and if it fails, release the node latch and
+     * do over.
      */
     Lock sharedCommitLock() {
         return mSharedCommitLock;
