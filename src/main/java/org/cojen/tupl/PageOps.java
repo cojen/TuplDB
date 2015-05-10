ea1348de54c48bd0fe7caf40abdcafbb092bd602
broneill <bronee@gmail.com>
2015-05-10 11:26:10 -0700

    Add page annotations.

--- src/main/java/org/cojen/tupl/PageOps.java
+++ src/main/java/org/cojen/tupl/PageOps.java
@@ -35,81 +35,81 @@ import static org.cojen.tupl.Utils.*;
  * @author Brian S O'Neill
  */
 final class PageOps {
-    static byte[] p_null() {
+    static /*P*/ byte[] p_null() {
         return null;
     }
 
-    static byte[] p_empty() {
+    static /*P*/ byte[] p_empty() {
         return EMPTY_BYTES;
     }
 
-    static byte[] p_alloc(int size) {
+    static /*P*/ byte[] p_alloc(int size) {
         return new byte[size];
     }
 
-    static byte[] p_calloc(int size) {
+    static /*P*/ byte[] p_calloc(int size) {
         return new byte[size];
     }
 
-    static byte[][] p_allocArray(int size) {
+    static /*P*/ byte[][] p_allocArray(int size) {
         return new byte[size][];
     }
 
-    static void p_delete(byte[] page) {
+    static void p_delete(/*P*/ byte[] page) {
     }
 
-    static byte[] p_clone(byte[] page) {
+    static /*P*/ byte[] p_clone(/*P*/ byte[] page) {
         return page.clone();
     }
 
     /**
      * Allocates a clone if the page type is not an array. Must be deleted.
      */
-    static byte[] p_transfer(byte[] array) {
+    static /*P*/ byte[] p_transfer(byte[] array) {
         return array;
     }
 
-    static int p_length(byte[] page) {
+    static int p_length(/*P*/ byte[] page) {
         return page.length;
     }
 
-    static byte p_byteGet(byte[] page, int index) {
+    static byte p_byteGet(/*P*/ byte[] page, int index) {
         return page[index];
     }
 
-    static int p_ubyteGet(byte[] page, int index) {
+    static int p_ubyteGet(/*P*/ byte[] page, int index) {
         return page[index] & 0xff;
     }
 
-    static void p_bytePut(byte[] page, int index, byte v) {
+    static void p_bytePut(/*P*/ byte[] page, int index, byte v) {
         page[index] = v;
     }
 
-    static void p_bytePut(byte[] page, int index, int v) {
+    static void p_bytePut(/*P*/ byte[] page, int index, int v) {
         page[index] = (byte) v;
     }
 
-    static int p_ushortGetLE(byte[] page, int index) {
+    static int p_ushortGetLE(/*P*/ byte[] page, int index) {
         return decodeUnsignedShortLE(page, index);
     }
 
-    static void p_shortPutLE(byte[] page, int index, int v) {
+    static void p_shortPutLE(/*P*/ byte[] page, int index, int v) {
         encodeShortLE(page, index, v);
     }
 
-    static int p_intGetLE(byte[] page, int index) {
+    static int p_intGetLE(/*P*/ byte[] page, int index) {
         return decodeIntLE(page, index);
     }
 
-    static void p_intPutLE(byte[] page, int index, int v) {
+    static void p_intPutLE(/*P*/ byte[] page, int index, int v) {
         encodeIntLE(page, index, v);
     }
 
-    static int p_uintGetVar(byte[] page, int index) {
+    static int p_uintGetVar(/*P*/ byte[] page, int index) {
         return decodeUnsignedVarInt(page, index);
     }
 
-    static int p_uintPutVar(byte[] page, int index, int v) {
+    static int p_uintPutVar(/*P*/ byte[] page, int index, int v) {
         return encodeUnsignedVarInt(page, index, v);
     }
 
@@ -117,35 +117,35 @@ final class PageOps {
         return calcUnsignedVarIntLength(v);
     }
 
-    static long p_uint48GetLE(byte[] page, int index) {
+    static long p_uint48GetLE(/*P*/ byte[] page, int index) {
         return decodeUnsignedInt48LE(page, index);
     }
 
-    static void p_int48PutLE(byte[] page, int index, long v) {
+    static void p_int48PutLE(/*P*/ byte[] page, int index, long v) {
         encodeInt48LE(page, index, v);
     }
 
-    static long p_longGetLE(byte[] page, int index) {
+    static long p_longGetLE(/*P*/ byte[] page, int index) {
         return decodeLongLE(page, index);
     }
 
-    static void p_longPutLE(byte[] page, int index, long v) {
+    static void p_longPutLE(/*P*/ byte[] page, int index, long v) {
         encodeLongLE(page, index, v);
     }
 
-    static long p_longGetBE(byte[] page, int index) {
+    static long p_longGetBE(/*P*/ byte[] page, int index) {
         return decodeLongBE(page, index);
     }
 
-    static void p_longPutBE(byte[] page, int index, long v) {
+    static void p_longPutBE(/*P*/ byte[] page, int index, long v) {
         encodeLongBE(page, index, v);
     }
 
-    static long p_ulongGetVar(byte[] page, IntegerRef ref) {
+    static long p_ulongGetVar(/*P*/ byte[] page, IntegerRef ref) {
         return decodeUnsignedVarLong(page, ref);
     }
 
-    static int p_ulongPutVar(byte[] page, int index, long v) {
+    static int p_ulongPutVar(/*P*/ byte[] page, int index, long v) {
         return encodeUnsignedVarLong(page, index, v);
     }
 
@@ -153,38 +153,44 @@ final class PageOps {
         return calcUnsignedVarLongLength(v);
     }
 
-    static void p_clear(byte[] page) {
+    static void p_clear(/*P*/ byte[] page) {
         java.util.Arrays.fill(page, (byte) 0);
     }
 
-    static void p_clear(byte[] page, int fromIndex, int toIndex) {
+    static void p_clear(/*P*/ byte[] page, int fromIndex, int toIndex) {
         java.util.Arrays.fill(page, fromIndex, toIndex, (byte) 0);
     }
 
     /**
      * Returns page if it's an array, else copies to given array and returns that.
      */
-    static byte[] p_copyIfNotArray(byte[] page, byte[] array) {
+    static /*P*/ byte[] p_copyIfNotArray(/*P*/ byte[] page, byte[] array) {
         return page;
     }
 
-    static void p_copyFromArray(byte[] src, int srcStart, byte[] dstPage, int dstStart, int len) {
+    static void p_copyFromArray(byte[] src, int srcStart,
+                                /*P*/ byte[] dstPage, int dstStart, int len)
+    {
         System.arraycopy(src, srcStart, dstPage, dstStart, len);
     }
 
-    static void p_copyToArray(byte[] srcPage, int srcStart, byte[] dst, int dstStart, int len) {
+    static void p_copyToArray(/*P*/ byte[] srcPage, int srcStart,
+                              byte[] dst, int dstStart, int len)
+    {
         System.arraycopy(srcPage, srcStart, dst, dstStart, len);
     }
 
-    static void p_copyFromBB(ByteBuffer src, byte[] dstPage, int dstStart, int len) {
+    static void p_copyFromBB(ByteBuffer src, /*P*/ byte[] dstPage, int dstStart, int len) {
         src.get(dstPage, dstStart, len);
     }
 
-    static void p_copyToBB(byte[] srcPage, int srcStart, ByteBuffer dst, int len) {
+    static void p_copyToBB(/*P*/ byte[] srcPage, int srcStart, ByteBuffer dst, int len) {
         dst.put(srcPage, srcStart, len);
     }
 
-    static void p_copy(byte[] srcPage, int srcStart, byte[] dstPage, int dstStart, int len) {
+    static void p_copy(/*P*/ byte[] srcPage, int srcStart,
+                       /*P*/ byte[] dstPage, int dstStart, int len)
+    {
         System.arraycopy(srcPage, srcStart, dstPage, dstStart, len);
     }
 
@@ -192,7 +198,7 @@ final class PageOps {
      * Performs multiple array copies, correctly ordered to prevent clobbering. The copies
      * must not overlap, and start1 must be less than start2.
      */
-    static void p_copies(byte[] page,
+    static void p_copies(/*P*/ byte[] page,
                          int start1, int dest1, int length1,
                          int start2, int dest2, int length2)
     {
@@ -209,7 +215,7 @@ final class PageOps {
      * Performs multiple array copies, correctly ordered to prevent clobbering. The copies
      * must not overlap, start1 must be less than start2, and start2 be less than start3.
      */
-    static void p_copies(byte[] page,
+    static void p_copies(/*P*/ byte[] page,
                          int start1, int dest1, int length1,
                          int start2, int dest2, int length2,
                          int start3, int dest3, int length3)
@@ -223,32 +229,32 @@ final class PageOps {
         }
     }
 
-    static int p_compareKeysPageToArray(byte[] apage, int aoff, int alen,
+    static int p_compareKeysPageToArray(/*P*/ byte[] apage, int aoff, int alen,
                                         byte[] b, int boff, int blen)
     {
         return compareKeys(apage, aoff, alen, b, boff, blen);
     }
 
-    static int p_compareKeysPageToPage(byte[] apage, int aoff, int alen,
-                                       byte[] bpage, int boff, int blen)
+    static int p_compareKeysPageToPage(/*P*/ byte[] apage, int aoff, int alen,
+                                       /*P*/ byte[] bpage, int boff, int blen)
     {
         return compareKeys(apage, aoff, alen, bpage, boff, blen);
     }
 
-    static byte[] p_midKeyLowPage(byte[] lowPage, int lowOff, int lowLen,
-                                  byte[] high, int highOff, int highLen)
+    static byte[] p_midKeyLowPage(/*P*/ byte[] lowPage, int lowOff, int lowLen,
+                                  /*P*/ byte[] high, int highOff, int highLen)
     {
         return midKey(lowPage, lowOff, lowLen, high, highOff, highLen);
     }
 
     static byte[] p_midKeyHighPage(byte[] low, int lowOff, int lowLen,
-                                   byte[] highPage, int highOff, int highLen)
+                                   /*P*/ byte[] highPage, int highOff, int highLen)
     {
         return midKey(low, lowOff, lowLen, highPage, highOff, highLen);
     }
 
-    static byte[] p_midKeyLowHighPage(byte[] lowPage, int lowOff, int lowLen,
-                                      byte[] highPage, int highOff, int highLen)
+    static byte[] p_midKeyLowHighPage(/*P*/ byte[] lowPage, int lowOff, int lowLen,
+                                      /*P*/ byte[] highPage, int highOff, int highLen)
     {
         return midKey(lowPage, lowOff, lowLen, highPage, highOff, highLen);
     }
