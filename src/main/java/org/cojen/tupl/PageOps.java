1a2a956523630342dd75b3ec4b66b2099508ede1
broneill <broneill@users.noreply.github.com>
2015-11-26 09:36:11 -0800

    Update copyright.

--- src/main/java/org/cojen/tupl/PageOps.java
+++ src/main/java/org/cojen/tupl/PageOps.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2015 Brian S O'Neill
+ *  Copyright 2015 Cojen.org
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
