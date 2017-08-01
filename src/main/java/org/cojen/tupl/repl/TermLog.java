b7b704b77054d389c962af4734b19aebbf3aa9c3
Brian S. O'Neill <bronee@gmail.com>
2017-08-01 09:41:50 -0700

    Revised return values when waiting for a commit index.

--- src/main/java/org/cojen/tupl/repl/TermLog.java
+++ src/main/java/org/cojen/tupl/repl/TermLog.java
@@ -67,8 +67,8 @@ interface TermLog extends LKey<TermLog>, Closeable {
      * Blocks until the commit index reaches the given index.
      *
      * @param nanosTimeout relative nanosecond time to wait; infinite if &lt;0
-     * @return current commit index, or -1 if timed out or if term finished before the index
-     * could be reached, or MIN_VALUE if closed
+     * @return current commit index, or -1 if term finished before the index could be reached,
+     * or -2 if timed out, or MIN_VALUE if closed
      */
     long waitForCommit(long index, long nanosTimeout) throws IOException;
 
