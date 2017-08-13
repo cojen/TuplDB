8ffb2d82e9a787c2194ec7f26f535f0851752a7e
Brian S. O'Neill <bronee@gmail.com>
2017-08-13 14:02:40 -0700

    Detect if a database restore operation failed, preventing the partially restored database from being opened.

--- src/main/java/org/cojen/tupl/_DurablePageDb.java
+++ src/main/java/org/cojen/tupl/_DurablePageDb.java
@@ -79,6 +79,7 @@ final class _DurablePageDb extends _PageDb {
     */
 
     private static final long MAGIC_NUMBER = 6529720411368701212L;
+    private static final long INCOMPLETE_RESTORE = 1295383629602457917L;
 
     // Indexes of entries in header node.
     private static final int I_MAGIC_NUMBER     = 0;
@@ -163,11 +164,19 @@ final class _DurablePageDb extends _PageDb {
         }
 
         PageArray[] arrays = new PageArray[files.length];
-        for (int i=0; i<files.length; i++) {
-            arrays[i] = new FilePageArray(pageSize, files[i], factory, options);
-        }
 
-        return new StripedPageArray(arrays);
+        try {
+            for (int i=0; i<files.length; i++) {
+                arrays[i] = new FilePageArray(pageSize, files[i], factory, options);
+            }
+
+            return new StripedPageArray(arrays);
+        } catch (Throwable e) {
+            for (PageArray pa : arrays) {
+                closeQuietly(null, pa);
+            }
+            throw e;
+        }
     }
 
     private static void checkPageSize(int pageSize) {
@@ -236,6 +245,8 @@ final class _DurablePageDb extends _PageDb {
                             commitNumber0 = p_intGetLE(header0, I_COMMIT_NUMBER);
                             pageSize0 = p_intGetLE(header0, I_PAGE_SIZE);
                             ex0 = null;
+                        } catch (IncompleteRestoreException e) {
+                            throw e;
                         } catch (CorruptDatabaseException e) {
                             header0 = p_null();
                             commitNumber0 = -1;
@@ -250,6 +261,8 @@ final class _DurablePageDb extends _PageDb {
                         try {
                             header1 = readHeader(1);
                             commitNumber1 = p_intGetLE(header1, I_COMMIT_NUMBER);
+                        } catch (IncompleteRestoreException e) {
+                            throw e;
                         } catch (CorruptDatabaseException e) {
                             if (ex0 != null) {
                                 // File is completely unusable.
@@ -703,58 +716,53 @@ final class _DurablePageDb extends _PageDb {
                                       PageCache cache, Crypto crypto, InputStream in)
         throws IOException
     {
-        if (options.contains(OpenOption.READ_ONLY)) {
-            throw new DatabaseException("Cannot restore into a read-only file");
-        }
-
-        byte[] buffer;
-        long bufferPage;
-        PageArray pa;
-        long index = 0;
-
-        if (crypto != null) {
-            buffer = new byte[pageSize];
-            bufferPage = p_transfer(buffer);
-            pa = openPageArray(pageSize, files, factory, options);
-            if (!pa.isEmpty()) {
-                throw new DatabaseException("Cannot restore into a non-empty file");
-            }
-        } else {
-            // Figure out what the actual page size is.
-
-            buffer = new byte[MINIMUM_PAGE_SIZE];
-            readFully(in, buffer, 0, buffer.length);
-
-            long magic = decodeLongLE(buffer, I_MAGIC_NUMBER);
-            if (magic != MAGIC_NUMBER) {
-                throw new CorruptDatabaseException("Wrong magic number: " + magic);
+        try {
+            if (options.contains(OpenOption.READ_ONLY)) {
+                throw new DatabaseException("Cannot restore into a read-only file");
             }
 
+            byte[] buffer;
+
+            if (crypto != null) {
+                buffer = new byte[pageSize];
+                readFully(in, buffer, 0, buffer.length);
+                try {
+                    crypto.decryptPage(0, pageSize, buffer, 0);
+                } catch (GeneralSecurityException e) {
+                    throw new DatabaseException(e);
+                }
+            } else {
+                // Start the with minimum page size and figure out what the actual size is.
+                buffer = new byte[MINIMUM_PAGE_SIZE];
+                readFully(in, buffer, 0, buffer.length);
+            }
+
+            checkMagicNumber(decodeLongLE(buffer, I_MAGIC_NUMBER));
             pageSize = decodeIntLE(buffer, I_PAGE_SIZE);
-            pa = openPageArray(pageSize, files, factory, options);
+            PageArray pa = openPageArray(pageSize, files, factory, options);
 
             if (!pa.isEmpty()) {
+                closeQuietly(null, pa);
                 throw new DatabaseException("Cannot restore into a non-empty file");
             }
 
             if (pageSize != buffer.length) {
-                byte[] newBuffer = new byte[pageSize];
-                arraycopy(buffer, 0, newBuffer, 0, buffer.length);
-                readFully(in, newBuffer, buffer.length, pageSize - buffer.length);
-                buffer = newBuffer;
+                if (crypto != null) {
+                    closeQuietly(null, pa);
+                    throw new CorruptDatabaseException
+                        ("Mismatched page size: " + pageSize + " != " + buffer.length);
+                } else {
+                    byte[] newBuffer = new byte[pageSize];
+                    arraycopy(buffer, 0, newBuffer, 0, buffer.length);
+                    readFully(in, newBuffer, buffer.length, pageSize - buffer.length);
+                    buffer = newBuffer;
+                }
             }
 
-            bufferPage = p_transfer(buffer);
-            try {
-                pa.writePage(index, bufferPage);
-                index++;
-            } catch (Throwable e) {
-                p_delete(bufferPage);
-                throw e;
-            }
+            return restoreFromSnapshot(cache, crypto, in, buffer, pa);
+        } finally {
+            closeQuietly(null, in);
         }
-
-        return restoreFromSnapshot(cache, crypto, in, buffer, bufferPage, pa, index);
     }
 
     /**
@@ -765,38 +773,104 @@ final class _DurablePageDb extends _PageDb {
     static _PageDb restoreFromSnapshot(PageArray pa, PageCache cache, Crypto crypto, InputStream in)
         throws IOException
     {
-        if (!pa.isEmpty()) {
-            throw new DatabaseException("Cannot restore into a non-empty file");
+        try {
+            if (!pa.isEmpty()) {
+                closeQuietly(null, pa);
+                throw new DatabaseException("Cannot restore into a non-empty file");
+            }
+
+            byte[] buffer = new byte[pa.pageSize()];
+            readFully(in, buffer, 0, buffer.length);
+
+            try {
+                crypto.decryptPage(0, buffer.length, buffer, 0);
+            } catch (GeneralSecurityException e) {
+                throw new DatabaseException(e);
+            }
+
+            checkMagicNumber(decodeLongLE(buffer, I_MAGIC_NUMBER));
+            int pageSize = decodeIntLE(buffer, I_PAGE_SIZE);
+
+            if (pageSize != buffer.length) {
+                closeQuietly(null, pa);
+                throw new CorruptDatabaseException
+                    ("Mismatched page size: " + pageSize + " != " + buffer.length);
+            }
+
+            return restoreFromSnapshot(cache, crypto, in, buffer, pa);
+        } finally {
+            closeQuietly(null, in);
         }
-
-        byte[] buffer = new byte[pa.pageSize()];
-        long bufferPage = p_transfer(buffer);
-
-        return restoreFromSnapshot(cache, crypto, in, buffer, bufferPage, pa, 0);
     }
 
+    /**
+     * @param buffer initialized with page 0 (first header)
+     */
     private static _PageDb restoreFromSnapshot(PageCache cache, Crypto crypto, InputStream in,
-                                              byte[] buffer, long bufferPage,
-                                              PageArray pa, long index)
+                                              byte[] buffer, PageArray pa)
         throws IOException
     {
+        // Indicate that a restore is in progress. Replace with the correct magic number when
+        // complete.
+        encodeLongLE(buffer, I_MAGIC_NUMBER, INCOMPLETE_RESTORE);
+
+        if (crypto != null) {
+            try {
+                crypto.encryptPage(0, pa.pageSize(), buffer, 0);
+            } catch (GeneralSecurityException e) {
+                closeQuietly(null, pa);
+                throw new DatabaseException(e);
+            }
+        }
+
+        long bufferPage = p_transfer(buffer);
+
         try {
+            // Write header and ensure that the incomplete restore state is persisted.
+            pa.writePage(0, bufferPage);
+            pa.sync(false);
+
+            long index = 1;
             while (true) {
-                try {
-                    readFully(in, buffer, 0, buffer.length);
-                } catch (EOFException e) {
+                int amt = in.read(buffer);
+                if (amt < 0) {
                     break;
                 }
+                readFully(in, buffer, amt, buffer.length - amt);
                 pa.writePage(index, p_transferTo(buffer, bufferPage));
                 index++;
             }
 
+            // Store proper magic number, indicating that the restore is complete. All data
+            // pages must be durable before doing this.
+
+            pa.sync(false);
+            pa.readPage(0, bufferPage);
+
+            try {
+                if (crypto != null) {
+                    crypto.decryptPage(0, pa.pageSize(), bufferPage, 0);
+                }
+
+                p_longPutLE(bufferPage, I_MAGIC_NUMBER, MAGIC_NUMBER);
+
+                if (crypto != null) {
+                    crypto.encryptPage(0, pa.pageSize(), bufferPage, 0);
+                }
+            } catch (GeneralSecurityException e) {
+                throw new DatabaseException(e);
+            }
+
+            pa.writePage(0, bufferPage);
+
             // Ensure newly restored snapshot is durable and also ensure that PageArray (if a
             // MappedPageArray) no longer considers itself to be empty.
             pa.sync(true);
+        } catch (Throwable e) {
+            closeQuietly(null, pa);
+            throw e;
         } finally {
             p_delete(bufferPage);
-            closeQuietly(null, in);
         }
 
         try {
@@ -875,10 +949,7 @@ final class _DurablePageDb extends _PageDb {
                 throw new CorruptDatabaseException("File is smaller than expected");
             }
 
-            long magic = p_longGetLE(header, I_MAGIC_NUMBER);
-            if (magic != MAGIC_NUMBER) {
-                throw new CorruptDatabaseException("Wrong magic number: " + magic);
-            }
+            checkMagicNumber(p_longGetLE(header, I_MAGIC_NUMBER));
 
             int checksum = p_intGetLE(header, I_CHECKSUM);
 
@@ -904,6 +975,15 @@ final class _DurablePageDb extends _PageDb {
             p_copyToArray(page, start, buf, offset, length);
         } finally {
             p_delete(page);
+        }
+    }
+
+    private static void checkMagicNumber(long magic) throws CorruptDatabaseException {
+        if (magic != MAGIC_NUMBER) {
+            if (magic == INCOMPLETE_RESTORE) {
+                throw new IncompleteRestoreException();
+            }
+            throw new CorruptDatabaseException("Wrong magic number: " + magic);
         }
     }
 }
