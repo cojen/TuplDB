1f76d815890d4f02fd04b3e0c32bd10ed533904f
Brian S. O'Neill <bronee@gmail.com>
2017-09-06 16:23:07 -0700

    Define a DirectReplicator interface which StreamReplicator and MessageReplicator extend.

--- src/main/java/org/cojen/tupl/repl/StreamReplicator.java
+++ src/main/java/org/cojen/tupl/repl/StreamReplicator.java
@@ -17,17 +17,12 @@
 
 package org.cojen.tupl.repl;
 
-import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 
-import java.net.ConnectException;
-import java.net.Socket;
 import java.net.SocketAddress;
 
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import java.util.function.Consumer;
@@ -54,7 +49,7 @@ import java.util.function.Consumer;
  * @author Brian S O'Neill
  * @see MessageReplicator
  */
-public interface StreamReplicator extends Replicator {
+public interface StreamReplicator extends DirectReplicator {
     /**
      * Open a replicator instance, creating it if necessary.
      *
@@ -102,66 +97,21 @@ public interface StreamReplicator extends Replicator {
     }
 
     /**
-     * Start accepting replication data, to be called for new or existing replicators.
-     *
-     * @return false if already started
+     * {@inheritDoc}
      */
-    boolean start() throws IOException;
-
-    /**
-     * Start by receiving a {@link #requestSnapshot snapshot} from another group member,
-     * expected to be called only by newly joined members.
-     *
-     * @param options requested options; can pass null if none
-     * @return null if no snapshot could be found and replicator hasn't started
-     * @throws ConnectException if a snapshot was found, but requesting it failed
-     * @throws IllegalStateException if already started
-     */
-    SnapshotReceiver restore(Map<String, String> options) throws IOException;
-
-    /**
-     * Returns a new reader which accesses data starting from the given index. The reader
-     * returns EOF whenever the end of a term is reached. At the end of a term, try to obtain a
-     * new writer to determine if the local member has become the leader.
-     *
-     * <p>When passing true for the follow parameter, a reader is always provided at the
-     * requested index. When passing false for the follow parameter, null is returned if the
-     * current member is the leader for the given index.
-     *
-     * <p><b>Note: Reader instances are not expected to be thread-safe.</b>
-     *
-     * @param index index to start reading from, known to have been committed
-     * @param follow pass true to obtain an active reader, even if local member is the leader
-     * @return reader or possibly null when follow is false
-     * @throws IllegalStateException if index is lower than the start index
-     */
+    @Override
     Reader newReader(long index, boolean follow);
 
     /**
-     * Returns a new writer for the leader to write into, or else returns null if the local
-     * member isn't the leader. The writer stops accepting messages when the term has ended,
-     * and possibly another leader has been elected.
-     *
-     * <p><b>Note: Writer instances are not expected to be thread-safe.</b>
-     *
-     * @return writer or null if not the leader
-     * @throws IllegalStateException if an existing writer for the current term already exists
+     * {@inheritDoc}
      */
+    @Override
     Writer newWriter();
 
     /**
-     * Returns a new writer for the leader to write into, or else returns null if the local
-     * member isn't the leader. The writer stops accepting messages when the term has ended,
-     * and possibly another leader has been elected.
-     *
-     * <p><b>Note: Writer instances are not expected to be thread-safe.</b>
-     *
-     * @param index expected index to start writing from as leader; method returns null if
-     * index doesn't match
-     * @return writer or null if not the leader
-     * @throws IllegalArgumentException if given index is negative
-     * @throws IllegalStateException if an existing writer for the current term already exists
+     * {@inheritDoc}
      */
+    @Override
     Writer newWriter(long index);
 
     /**
@@ -190,28 +140,6 @@ public interface StreamReplicator extends Replicator {
      * @param acceptor acceptor to use, or pass null to disable
      */
     void controlMessageAcceptor(Consumer<byte[]> acceptor);
-
-    /**
-     * Connect to a remote replication group member, for receiving a database snapshot. An
-     * {@link #snapshotRequestAcceptor acceptor} must be installed on the group member being
-     * connected to for the request to succeed.
-     * 
-     * <p>The sender is selected as the one which has the fewest count of active snapshot
-     * sessions. If all the counts are the same, then a sender is instead randomly selected,
-     * favoring a follower over a leader.
-     *
-     * @param options requested options; can pass null if none
-     * @return null if no snapshot could be found
-     * @throws ConnectException if a snapshot was found, but requesting it failed
-     */
-    SnapshotReceiver requestSnapshot(Map<String, String> options) throws IOException;
-
-    /**
-     * Install a callback to be invoked when a snapshot is requested by a new group member.
-     *
-     * @param acceptor acceptor to use, or pass null to disable
-     */
-    void snapshotRequestAcceptor(Consumer<SnapshotSender> acceptor);
 
     public static interface Reader extends Replicator.Reader {
         /**
