34abe0b1e9937ce0015275f0d9884a48e589f289
Brian S. O'Neill <bronee@gmail.com>
2017-10-07 11:02:39 -0700

    Add replication debug event type. Also, debug events should log as Level.FINE.

--- src/main/java/org/cojen/tupl/EventType.java
+++ src/main/java/org/cojen/tupl/EventType.java
@@ -27,7 +27,7 @@ import java.util.logging.Level;
  */
 public enum EventType {
     /** General debug event type. */
-    DEBUG(Category.DEBUG, Level.INFO),
+    DEBUG(Category.DEBUG, Level.FINE),
 
     /** Signals the beginning of cache initialization. */
     CACHE_INIT_BEGIN(Category.CACHE_INIT, Level.INFO),
@@ -62,6 +62,8 @@ public enum EventType {
 
     /** Snapshot restore progress event. */
     REPLICATION_RESTORE(Category.REPLICATION, Level.INFO),
+    /** Generic debug message from the replication system. */
+    REPLICATION_DEBUG(Category.REPLICATION, Level.FINE),
     /** Generic info message from the replication system. */
     REPLICATION_INFO(Category.REPLICATION, Level.INFO),
     /** Generic warning message from the replication system. */
