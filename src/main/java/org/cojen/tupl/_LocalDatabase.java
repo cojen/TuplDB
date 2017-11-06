b3f8c4c348087d7586a2880c3d120557d7446760
Brian S. O'Neill <bronee@gmail.com>
2017-11-05 18:58:01 -0800

    Remove FIXME comments. Partial truncation isn't possible anymore, now that deletes are forced.

--- src/main/java/org/cojen/tupl/_LocalDatabase.java
+++ src/main/java/org/cojen/tupl/_LocalDatabase.java
@@ -4441,8 +4441,6 @@ final class _LocalDatabase extends AbstractDatabase {
 
         if ((header & 0x01) == 0) {
             // Direct pointers.
-            // FIXME: Delete in reverse order. If deleteFragment method fails, update the
-            // resulting value length, accounting for any inline content.
             while (len >= 6) {
                 long nodeId = p_uint48GetLE(fragmented, off);
                 off += 6;
@@ -4477,8 +4475,6 @@ final class _LocalDatabase extends AbstractDatabase {
         for (int poffset = 0, i=0; i<childNodeCount; poffset += 6, i++) {
             childNodeIds[i] = p_uint48GetLE(page, poffset);
         }
-        // FIXME: Delete in reverse order, incrementally. If deleteNode method fails, update
-        // the resulting value length, accounting for any inline content.
         deleteNode(inode);
 
         if (level <= 0) for (long childNodeId : childNodeIds) {
