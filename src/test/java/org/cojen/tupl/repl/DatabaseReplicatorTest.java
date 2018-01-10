4bd6bfda3197c97ee79f00ccac32d3de2674fa22
Brian S. O'Neill <bronee@gmail.com>
2018-01-10 09:05:09 -0800

    Test that replicated prepared transaction is handed off to the next leader.

--- src/test/java/org/cojen/tupl/repl/DatabaseReplicatorTest.java
+++ src/test/java/org/cojen/tupl/repl/DatabaseReplicatorTest.java
@@ -18,14 +18,29 @@
 package org.cojen.tupl.repl;
 
 import java.io.File;
+import java.io.IOException;
 
 import java.net.ServerSocket;
 
+import java.util.Arrays;
+
+import java.util.concurrent.TransferQueue;
+import java.util.concurrent.LinkedTransferQueue;
+
+import java.util.function.Supplier;
+
+import org.cojen.tupl.Cursor;
 import org.cojen.tupl.Database;
 import org.cojen.tupl.DatabaseConfig;
 import org.cojen.tupl.EventPrinter;
 import org.cojen.tupl.Index;
+import org.cojen.tupl.LockResult;
+import org.cojen.tupl.Transaction;
 import org.cojen.tupl.UnmodifiableReplicaException;
+
+import org.cojen.tupl.ext.RecoveryHandler;
+
+import org.cojen.tupl.io.Utils;
 
 import org.cojen.tupl.TestUtils;
 
@@ -62,6 +77,7 @@ public class DatabaseReplicatorTest {
     private File[] mReplBaseFiles;
     private int[] mReplPorts;
     private ReplicatorConfig[] mReplConfigs;
+    private DatabaseReplicator[] mReplicators;
     private DatabaseConfig[] mDbConfigs;
     private Database[] mDatabases;
 
@@ -69,13 +85,16 @@ public class DatabaseReplicatorTest {
      * @return first is the leader
      */
     private Database[] startGroup(int members) throws Exception {
-        return startGroup(members, Role.NORMAL);
+        return startGroup(members, Role.NORMAL, null);
     }
 
     /**
      * @return first is the leader
      */
-    private Database[] startGroup(int members, Role replicaRole) throws Exception {
+    private Database[] startGroup(int members, Role replicaRole,
+                                  Supplier<RecoveryHandler> handlerSupplier)
+        throws Exception
+    {
         if (members < 1) {
             throw new IllegalArgumentException();
         }
@@ -89,6 +108,7 @@ public class DatabaseReplicatorTest {
         mReplBaseFiles = new File[members];
         mReplPorts = new int[members];
         mReplConfigs = new ReplicatorConfig[members];
+        mReplicators = new DatabaseReplicator[members];
         mDbConfigs = new DatabaseConfig[members];
         mDatabases = new Database[members];
 
@@ -98,18 +118,25 @@ public class DatabaseReplicatorTest {
 
             mReplConfigs[i] = new ReplicatorConfig()
                 .groupToken(1)
-                .localSocket(sockets[i]);
+                .localSocket(sockets[i])
+                .baseFile(mReplBaseFiles[i]);
 
             if (i > 0) {
                 mReplConfigs[i].addSeed(sockets[0].getLocalSocketAddress());
                 mReplConfigs[i].localRole(replicaRole);
             }
 
+            mReplicators[i] = DatabaseReplicator.open(mReplConfigs[i]);
+
             mDbConfigs[i] = new DatabaseConfig()
                 .baseFile(mReplBaseFiles[i])
-                .replicate(mReplConfigs[i])
+                .replicate(mReplicators[i])
                 //.eventListener(new EventPrinter())
                 .directPageAccess(false);
+
+            if (handlerSupplier != null) {
+                mDbConfigs[i].recoveryHandler(handlerSupplier.get());
+            }
 
             Database db = Database.open(mDbConfigs[i]);
             mDatabases[i] = db;
@@ -198,6 +225,109 @@ public class DatabaseReplicatorTest {
 
                     TestUtils.sleep(100);
                 }
+            }
+        }
+    }
+
+    @Test
+    public void txnPrepare() throws Exception {
+        // Test that unfinished prepared transactions are passed to the new leader.
+
+        TransferQueue<Database> recovered = new LinkedTransferQueue<>();
+
+        Supplier<RecoveryHandler> supplier = () -> new RecoveryHandler() {
+            private Database mDb;
+
+            @Override
+            public void init(Database db) {
+                mDb = db;
+            }
+
+            @Override
+            public void recover(Transaction txn) throws IOException {
+                try {
+                    recovered.transfer(mDb);
+
+                    // Wait for the signal...
+                    recovered.take();
+
+                    // Modify the value before committing.
+                    Index ix = mDb.openIndex("test");
+                    Cursor c = ix.newCursor(txn);
+                    c.find("hello".getBytes());
+                    byte[] newValue = Arrays.copyOfRange(c.value(), 0, c.value().length + 1);
+                    newValue[newValue.length - 1] = '!';
+                    c.store(newValue);
+                    c.reset();
+
+                    txn.commit();
+                } catch (Exception e) {
+                    throw Utils.rethrow(e);
+                }
+            }
+        };
+
+        final int memberCount = 3;
+        Database[] dbs = startGroup(memberCount, Role.NORMAL, supplier);
+
+        Index ix0 = dbs[0].openIndex("test");
+
+        // Wait for all members to be electable.
+        allElectable: {
+            int count = 0;
+            for (int i=0; i<100; i++) {
+                count = 0;
+                for (DatabaseReplicator repl : mReplicators) {
+                    if (repl.getLocalRole() == Role.NORMAL) {
+                        count++;
+                    }
+                }
+                if (count >= mReplicators.length) {
+                    break allElectable;
+                }
+                Thread.sleep(100);
+            }
+
+            fail("Not all members are electable: " + count);
+        }
+
+        Transaction txn = dbs[0].newTransaction();
+        byte[] key = "hello".getBytes();
+        ix0.store(txn, key, "world".getBytes());
+        txn.prepare();
+
+        // Close the leader and verify handoff.
+        dbs[0].close();
+
+        Database db = recovered.take();
+        assertNotEquals(dbs[0], db);
+
+        // Still locked.
+        Index ix = db.openIndex("test");
+        txn = db.newTransaction();
+        assertEquals(LockResult.TIMED_OUT_LOCK, ix.tryLockShared(txn, key, 0));
+        txn.reset();
+
+        // Signal that the handler can finish the transaction.
+        recovered.add(db);
+
+        assertArrayEquals("world!".getBytes(), ix.load(null, key));
+
+        // Verify replication.
+        Database remaining = dbs[1];
+        if (remaining == db) {
+            remaining = dbs[2];
+        }
+        ix = remaining.openIndex("test");
+        for (int i=10; --i>=0; ) {
+            try {
+                assertArrayEquals("world!".getBytes(), ix.load(null, key));
+                break;
+            } catch (Throwable e) {
+                if (i <= 0) {
+                    throw e;
+                }
+                Thread.sleep(100);
             }
         }
     }
