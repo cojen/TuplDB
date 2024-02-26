86934c232b65351d8f4dc39f57510a11ab6308ee
Brian S. O'Neill <bronee@gmail.com>
2024-02-26 11:10:42 -0800

    Method doesn't need to be public.

--- src/main/java/org/cojen/tupl/table/Converter.java
+++ src/main/java/org/cojen/tupl/table/Converter.java
@@ -71,9 +71,9 @@ public class Converter {
      *
      * @see #convertExact
      */
-    public static void convertLossy(final MethodMaker mm,
-                                    final ColumnInfo srcInfo, final Variable srcVar,
-                                    final ColumnInfo dstInfo, final Variable dstVar)
+    static void convertLossy(final MethodMaker mm,
+                             final ColumnInfo srcInfo, final Variable srcVar,
+                             final ColumnInfo dstInfo, final Variable dstVar)
     {
         if (srcInfo.isCompatibleWith(dstInfo)) {
             dstVar.set(srcVar);
