12178a926f12f8ad36fa3d9addd29dda048dfe23
Brian S. O'Neill <bronee@gmail.com>
2024-05-24 21:17:17 -0700

    Revert "Remove ReferenceQueue from MultiCache, since it ends up being publicly available. There's no significant benefit in cleaning up after cleared soft references anyhow, at least not with a polling technique. The cleared references get removed during a rehash, as before, except there's a possibility that the entry array grows a bit larger than necessary. This might reduce hash collisions anyhow, so it seems fine."
    This reverts commit 402a5bb4abf2f42d535357adc2b6869d2f326c62.

--- src/main/java/org/cojen/tupl/table/MultiCache.java
+++ src/main/java/org/cojen/tupl/table/MultiCache.java
@@ -20,6 +20,7 @@ package org.cojen.tupl.table;
 import java.lang.invoke.VarHandle;
 
 import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
 
 import java.util.function.Consumer;
@@ -31,13 +32,14 @@ import org.cojen.tupl.util.Latch;
  *
  * @author Brian S. O'Neill
  */
-public abstract class MultiCache<K, V, H, X extends Throwable> {
+public abstract class MultiCache<K, V, H, X extends Throwable> extends ReferenceQueue<Object> {
     public static sealed abstract class Type {
         abstract int hash(int hash);
 
         abstract <K> boolean matches(Entry<K, ?> entry, K key);
 
-        abstract <K, V> Entry<K, V> newEntry(K key, V value, int hash);
+        abstract <K, V> Entry<K, V> newEntry(K key, V value, int hash,
+                                             ReferenceQueue<Object> queue);
     }
 
     /** Cache key type typically used by Table.query implementations. */
@@ -106,6 +108,13 @@ public abstract class MultiCache<K, V, H, X extends Throwable> {
      * Double check with synchronization.
      */
     public final V cacheGet(Type type, K key) {
+        Object ref = poll();
+        if (ref != null) {
+            synchronized (this) {
+                cleanup(ref);
+            }
+        }
+
         var entries = mEntries;
         int hash = type.hash(key.hashCode());
         int index = hash & (entries.length - 1);
@@ -184,6 +193,11 @@ public abstract class MultiCache<K, V, H, X extends Throwable> {
 
     @SuppressWarnings("unchecked")
     public synchronized final void cachePut(Type type, K key, V value) {
+        Object ref = poll();
+        if (ref != null) {
+            cleanup(ref);
+        }
+
         var entries = mEntries;
         int hash = type.hash(key.hashCode());
         int index = hash & (entries.length - 1);
@@ -191,7 +205,7 @@ public abstract class MultiCache<K, V, H, X extends Throwable> {
         for (Entry<K, V> e = entries[index], prev = null; e != null; e = e.mNext) {
             if (type.matches(e, key)) {
                 e.clear();
-                var newEntry = type.newEntry(key, value, hash);
+                var newEntry = type.newEntry(key, value, hash, this);
                 if (prev == null) {
                     newEntry.mNext = e.mNext;
                 } else {
@@ -227,7 +241,7 @@ public abstract class MultiCache<K, V, H, X extends Throwable> {
             index = hash & (entries.length - 1);
         }
 
-        var newEntry = type.newEntry(key, value, hash);
+        var newEntry = type.newEntry(key, value, hash, this);
         newEntry.mNext = entries[index];
         VarHandle.storeStoreFence(); // ensure that entry value is safely visible
         entries[index] = newEntry;
@@ -253,6 +267,11 @@ public abstract class MultiCache<K, V, H, X extends Throwable> {
                 prev = e;
             }
         }
+
+        Object ref = poll();
+        if (ref != null) {
+            cleanup(ref);
+        }
     }
 
     /**
@@ -260,14 +279,41 @@ public abstract class MultiCache<K, V, H, X extends Throwable> {
      */
     protected abstract V cacheNewValue(Type type, K key, H helper) throws X;
 
+    /**
+     * Caller must be synchronized.
+     *
+     * @param ref not null
+     */
+    @SuppressWarnings({"unchecked"})
+    private void cleanup(Object ref) {
+        var entries = mEntries;
+        do {
+            var cleared = (Entry<K, V>) ref;
+            int ix = cleared.mHash & (entries.length - 1);
+            for (Entry<K, V> e = entries[ix], prev = null; e != null; e = e.mNext) {
+                if (e == cleared) {
+                    if (prev == null) {
+                        entries[ix] = e.mNext;
+                    } else {
+                        prev.mNext = e.mNext;
+                    }
+                    mSize--;
+                    break;
+                } else {
+                    prev = e;
+                }
+            }
+        } while ((ref = poll()) != null);
+    }
+
     private static sealed abstract class Entry<K, V> extends SoftReference<V> {
         final K mKey;
         final int mHash;
 
         Entry<K, V> mNext;
 
-        Entry(K key, V value, int hash) {
-            super(value);
+        Entry(K key, V value, int hash, ReferenceQueue<Object> queue) {
+            super(value, queue);
             mKey = key;
             mHash = hash;
         }
@@ -287,14 +333,14 @@ public abstract class MultiCache<K, V, H, X extends Throwable> {
         }
 
         @Override
-        <K, V> Entry<K, V> newEntry(K key, V value, int hash) {
-            return new LatchEntry<>(key, value, hash);
+        <K, V> Entry<K, V> newEntry(K key, V value, int hash, ReferenceQueue<Object> queue) {
+            return new LatchEntry<>(key, value, hash, queue);
         }
     }
 
     private static final class LatchEntry<K, V> extends Entry<K, V> {
-        LatchEntry(K key, V value, int hash) {
-            super(key, value, hash);
+        LatchEntry(K key, V value, int hash, ReferenceQueue<Object> queue) {
+            super(key, value, hash, queue);
         }
     }
 
@@ -310,14 +356,14 @@ public abstract class MultiCache<K, V, H, X extends Throwable> {
         }
 
         @Override
-        <K, V> Entry<K, V> newEntry(K key, V value, int hash) {
-            return new Type1Entry<>(key, value, hash);
+        <K, V> Entry<K, V> newEntry(K key, V value, int hash, ReferenceQueue<Object> queue) {
+            return new Type1Entry<>(key, value, hash, queue);
         }
     }
 
     private static final class Type1Entry<K, V> extends Entry<K, V> {
-        Type1Entry(K key, V value, int hash) {
-            super(key, value, hash);
+        Type1Entry(K key, V value, int hash, ReferenceQueue<Object> queue) {
+            super(key, value, hash, queue);
         }
     }
 
@@ -333,14 +379,14 @@ public abstract class MultiCache<K, V, H, X extends Throwable> {
         }
 
         @Override
-        <K, V> Entry<K, V> newEntry(K key, V value, int hash) {
-            return new Type2Entry<>(key, value, hash);
+        <K, V> Entry<K, V> newEntry(K key, V value, int hash, ReferenceQueue<Object> queue) {
+            return new Type2Entry<>(key, value, hash, queue);
         }
     }
 
     private static final class Type2Entry<K, V> extends Entry<K, V> {
-        Type2Entry(K key, V value, int hash) {
-            super(key, value, hash);
+        Type2Entry(K key, V value, int hash, ReferenceQueue<Object> queue) {
+            super(key, value, hash, queue);
         }
     }
 }
